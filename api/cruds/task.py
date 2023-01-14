0ab8d2774882eeb587d5a7c1ab1397252db90ea6
toms3776c <24806125+toms3776c@users.noreply.github.com>
2023-01-15 00:22:23 +0900

    taskデータ取得部分を実装

---
+++ api/cruds/task.py
@@ -0,0 +1,30 @@
+from typing import List, Tuple
+from sqlalchemy import select
+from sqlalchemy.engine import Result
+from sqlalchemy.ext.asyncio import AsyncSession
+
+import api.models.task as task_model
+import api.schemas.task as task_schema
+
+
+async def get_tasks_with_done(db: AsyncSession) -> List[Tuple[int, str, bool]]:
+    result: Result = await (
+        db.execute(
+            select(
+                task_model.Task.id,
+                task_model.Task.title,
+                task_model.Done.id.isnot(None).label("done"),
+            ).outerjoin(task_model.Done)
+        )
+    )
+    return result.all()
+
+
+async def create_task(
+    db: AsyncSession, task_create: task_schema.TaskCreate
+) -> task_model.Task:
+    task = task_model.Task(**task_create.dict())
+    db.add(task)
+    await db.commit()
+    await db.refresh(task)
+    return task
